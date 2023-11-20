be73fd6c6f73ce9b08bc558bad158554f49528fd
westkite05 <westkite23@naver.com>
2023-11-20 11:47:09 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,127 +8,127 @@ import {
   useContractMetadata,
   useTokenSupply,
   Web3Button,
-} from "@thirdweb-dev/react";
-import { BigNumber, utils } from "ethers";
-import Image from "next/image";
-import { useMemo, useState } from "react";
-import styles from "../styles/Home.module.css";
-import { parseIneligibility } from "../utils/parseIneligibility";
+} from "@thirdweb-dev/react"
+import { BigNumber, utils } from "ethers"
+import Image from "next/image"
+import { useMemo, useState } from "react"
+import styles from "../styles/Home.module.css"
+import { parseIneligibility } from "../utils/parseIneligibility"
 
 const Home = () => {
-  const tokenAddress = "0x03728725240b021887355c943d040BF933F3d5F0";
-  const { contract } = useContract(tokenAddress, "token-drop");
-  const address = useAddress();
-  const [quantity, setQuantity] = useState(1);
-  const { data: contractMetadata } = useContractMetadata(contract);
+  const tokenAddress = "0xe92C464232Fb52e1E3eaDEC9C26867801E8796D5"
+  const { contract } = useContract(tokenAddress, "token-drop")
+  const address = useAddress()
+  const [quantity, setQuantity] = useState(1)
+  const { data: contractMetadata } = useContractMetadata(contract)
 
-  const claimConditions = useClaimConditions(contract);
+  const claimConditions = useClaimConditions(contract)
   const activeClaimCondition = useActiveClaimConditionForWallet(
     contract,
     address
-  );
-  const claimerProofs = useClaimerProofs(contract, address || "");
+  )
+  const claimerProofs = useClaimerProofs(contract, address || "")
   const claimIneligibilityReasons = useClaimIneligibilityReasons(contract, {
     quantity,
     walletAddress: address || "",
-  });
+  })
 
-  const claimedSupply = useTokenSupply(contract);
+  const claimedSupply = useTokenSupply(contract)
 
   const totalAvailableSupply = useMemo(() => {
     try {
-      return BigNumber.from(activeClaimCondition.data?.availableSupply || 0);
+      return BigNumber.from(activeClaimCondition.data?.availableSupply || 0)
     } catch {
-      return BigNumber.from(1_000_000_000);
+      return BigNumber.from(1_000_000_000)
     }
-  }, [activeClaimCondition.data?.availableSupply]);
+  }, [activeClaimCondition.data?.availableSupply])
 
   const numberClaimed = useMemo(() => {
-    return BigNumber.from(claimedSupply.data?.value || 0).toString();
-  }, [claimedSupply]);
+    return BigNumber.from(claimedSupply.data?.value || 0).toString()
+  }, [claimedSupply])
 
   const numberTotal = useMemo(() => {
     const n = totalAvailableSupply.add(
       BigNumber.from(claimedSupply.data?.value || 0)
-    );
+    )
     if (n.gte(1_000_000_000)) {
-      return "";
+      return ""
     }
-    return n.toString();
-  }, [totalAvailableSupply, claimedSupply]);
+    return n.toString()
+  }, [totalAvailableSupply, claimedSupply])
 
   const priceToMint = useMemo(() => {
     if (quantity) {
       const bnPrice = BigNumber.from(
         activeClaimCondition.data?.currencyMetadata.value || 0
-      );
+      )
       return `${utils.formatUnits(
         bnPrice.mul(quantity).toString(),
         activeClaimCondition.data?.currencyMetadata.decimals || 18
-      )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
+      )} ${activeClaimCondition.data?.currencyMetadata.symbol}`
     }
   }, [
     activeClaimCondition.data?.currencyMetadata.decimals,
     activeClaimCondition.data?.currencyMetadata.symbol,
     activeClaimCondition.data?.currencyMetadata.value,
     quantity,
-  ]);
+  ])
 
   const maxClaimable = useMemo(() => {
-    let bnMaxClaimable;
+    let bnMaxClaimable
     try {
       bnMaxClaimable = BigNumber.from(
         activeClaimCondition.data?.maxClaimableSupply || 0
-      );
+      )
     } catch (e) {
-      bnMaxClaimable = BigNumber.from(1_000_000_000);
+      bnMaxClaimable = BigNumber.from(1_000_000_000)
     }
 
-    let perTransactionClaimable;
+    let perTransactionClaimable
     try {
       perTransactionClaimable = BigNumber.from(
         activeClaimCondition.data?.maxClaimablePerWallet || 0
-      );
+      )
     } catch (e) {
-      perTransactionClaimable = BigNumber.from(1_000_000_000);
+      perTransactionClaimable = BigNumber.from(1_000_000_000)
     }
 
     if (perTransactionClaimable.lte(bnMaxClaimable)) {
-      bnMaxClaimable = perTransactionClaimable;
+      bnMaxClaimable = perTransactionClaimable
     }
 
-    const snapshotClaimable = claimerProofs.data?.maxClaimable;
+    const snapshotClaimable = claimerProofs.data?.maxClaimable
 
     if (snapshotClaimable) {
       if (snapshotClaimable === "0") {
         // allowed unlimited for the snapshot
-        bnMaxClaimable = BigNumber.from(1_000_000_000);
+        bnMaxClaimable = BigNumber.from(1_000_000_000)
       } else {
         try {
-          bnMaxClaimable = BigNumber.from(snapshotClaimable);
+          bnMaxClaimable = BigNumber.from(snapshotClaimable)
         } catch (e) {
           // fall back to default case
         }
       }
     }
 
-    let max;
+    let max
     if (totalAvailableSupply.lt(bnMaxClaimable)) {
-      max = totalAvailableSupply;
+      max = totalAvailableSupply
     } else {
-      max = bnMaxClaimable;
+      max = bnMaxClaimable
     }
 
     if (max.gte(1_000_000_000)) {
-      return 1_000_000_000;
+      return 1_000_000_000
     }
-    return max.toNumber();
+    return max.toNumber()
   }, [
     claimerProofs.data?.maxClaimable,
     totalAvailableSupply,
     activeClaimCondition.data?.maxClaimableSupply,
     activeClaimCondition.data?.maxClaimablePerWallet,
-  ]);
+  ])
 
   const isSoldOut = useMemo(() => {
     try {
@@ -138,16 +138,16 @@ const Home = () => {
             0
           )) ||
         numberClaimed === numberTotal
-      );
+      )
     } catch (e) {
-      return false;
+      return false
     }
   }, [
     activeClaimCondition.data?.availableSupply,
     activeClaimCondition.isSuccess,
     numberClaimed,
     numberTotal,
-  ]);
+  ])
 
   const canClaim = useMemo(() => {
     return (
@@ -155,44 +155,44 @@ const Home = () => {
       claimIneligibilityReasons.isSuccess &&
       claimIneligibilityReasons.data?.length === 0 &&
       !isSoldOut
-    );
+    )
   }, [
     activeClaimCondition.isSuccess,
     claimIneligibilityReasons.data?.length,
     claimIneligibilityReasons.isSuccess,
     isSoldOut,
-  ]);
+  ])
 
   const isLoading = useMemo(() => {
-    return activeClaimCondition.isLoading || !contract;
-  }, [activeClaimCondition.isLoading, contract]);
+    return activeClaimCondition.isLoading || !contract
+  }, [activeClaimCondition.isLoading, contract])
 
   const buttonLoading = useMemo(
     () => isLoading || claimIneligibilityReasons.isLoading,
     [claimIneligibilityReasons.isLoading, isLoading]
-  );
+  )
   const buttonText = useMemo(() => {
     if (isSoldOut) {
-      return "Sold Out";
+      return "Sold Out"
     }
 
     if (canClaim) {
       const pricePerToken = BigNumber.from(
         activeClaimCondition.data?.currencyMetadata.value || 0
-      );
+      )
       if (pricePerToken.eq(0)) {
-        return "Mint (Free)";
+        return "Mint (Free)"
       }
-      return `Mint (${priceToMint})`;
+      return `Mint (${priceToMint})`
     }
     if (claimIneligibilityReasons.data?.length) {
-      return parseIneligibility(claimIneligibilityReasons.data, quantity);
+      return parseIneligibility(claimIneligibilityReasons.data, quantity)
     }
     if (buttonLoading) {
-      return "Checking eligibility...";
+      return "Checking eligibility..."
     }
 
-    return "Claiming not available";
+    return "Claiming not available"
   }, [
     isSoldOut,
     canClaim,
@@ -201,7 +201,7 @@ const Home = () => {
     activeClaimCondition.data?.currencyMetadata.value,
     priceToMint,
     quantity,
-  ]);
+  ])
 
   return (
     <div className={styles.container}>
@@ -242,6 +242,10 @@ const Home = () => {
         </>
       )}
 
+      <div>
+        Token Contract Address= 0xe92C464232Fb52e1E3eaDEC9C26867801E8796D5
+      </div>
+
       <hr className={styles.divider} />
 
       <div className={styles.claimGrid}>
@@ -249,13 +253,13 @@ const Home = () => {
           type="number"
           placeholder="Enter amount to claim"
           onChange={(e) => {
-            const value = parseInt(e.target.value);
+            const value = parseInt(e.target.value)
             if (value > maxClaimable) {
-              setQuantity(maxClaimable);
+              setQuantity(maxClaimable)
             } else if (value < 1) {
-              setQuantity(1);
+              setQuantity(1)
             } else {
-              setQuantity(value);
+              setQuantity(value)
             }
           }}
           value={quantity}
@@ -272,7 +276,7 @@ const Home = () => {
         </Web3Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
